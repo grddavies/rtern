1bb8a63f486a92d2a7726c30f0f7274acfa021a9
Gethin Davies <grd.davies@gmail.com>
2021-02-04 13:30:52 +0000

    Extend `?` to handle logical vectors

--- R/conditional_operator.R
+++ R/conditional_operator.R
@@ -2,10 +2,30 @@
 #'
 #' Who has time for if/else?
 #'
-#' @param lhs An expression that returns a logical value, or an assignment to an
-#'     expression which returns a logical.
+#' @param lhs A logical expression  or logical vector, or an assignment to an
+#'     a logical.
 #' @param rhs A pair of expressions separated by a colon (`:`).
+#' @return One of the values passed to the rhs, depending on truthiness of the lhs
+#' @examples
+#' # Conditional evaluation
+#' 4 > 3 ? "it_was_true" : "it_was_false"
+#' #> "it_was_true"
 #'
+#' FALSE ? "it_was_true" : "it_was_false"
+#' #> "it_was_false"
+#'
+#' # Vectorised evaluation
+#' c(4, 2) < 3 ? "it_was_true" : "it_was_false"
+#' #> "it_was_false" "it_was_true"
+#'
+#' # Conditional assignment
+#' x <- 4 > 3 ? "it_was_true" : "it_was_false"
+#' x
+#' #> "it_was_true"
+#'
+#' y = 3 > 4 ? "it_was_true" : "it_was_false"
+#' y
+#' #> "it_was_false"
 #' @export
 `?` <- function(lhs, rhs) {
   lexpr <- substitute(lhs, environment())
@@ -13,14 +33,21 @@
   lhs_tree <- as.list(lexpr)
 
   if (is_assignment(lhs_tree)) {
-    lexpr <- eval(lhs_tree[[3]])
+    lhs <- eval(lhs_tree[[3]])
   }
 
-
-  r <- eval(sapply(
+  rhss <- sapply(
     strsplit(deparse(rexpr), ":"),
     function(e) parse(text = e)
-  )[[2 - as.logical(lhs)]])
+  )
+
+  if (length(lhs) > 1) {
+    r <- sapply(lhs, function(x) {
+      eval(rhss[[2 - as.numeric(x)]])
+    })
+  } else {
+    r <- eval(rhss[[2 - as.numeric(lhs)]])
+  }
 
   if (is_assignment(lhs_tree)) {
     lhs_tree[[3]] <- r
--- tests/testthat/test-conditional_operator.R
+++ tests/testthat/test-conditional_operator.R
@@ -2,14 +2,14 @@ library(rtern)
 
 test_that("`?` evaluates simple TRUE cases correctly", {
   expect_equal(TRUE ? 1 : 2, 1)
-  expect_equal(TRUE ? c(1, 2) : c(9, 8), c(1, 2))
+  expect_equal(TRUE ? c(1, 2) : c(9, 8, 7), c(1, 2))
   expect_equal(TRUE ? 'this' : 'that', 'this')
   expect_equal(TRUE ? FALSE : TRUE, FALSE)
 })
 
 test_that("`?` evaluates simple TRUE expressions correctly", {
   expect_equal(3 > 1 ? 1 : 2, 1)
-  expect_equal(1 < 3 ? c(1, 2) : c(9, 8), c(1, 2))
+  expect_equal(1 < 3 ? c(1, 2) : c(9, 8, 7), c(1, 2))
   expect_equal(6 == 6 ? 'this' : 'that', 'this')
   expect_equal(is.character('a') ? FALSE : TRUE , FALSE)
 })
@@ -17,7 +17,7 @@ test_that("`?` evaluates simple TRUE expressions correctly", {
 
 test_that("`?` evaluates multi-part TRUE expressions correctly", {
   expect_equal(3 > 1 & is.numeric(1) ? 1 : 2, 1)
-  expect_equal(1 < 3 & !FALSE ? c(1, 2) : c(9, 8), c(1, 2))
+  expect_equal(1 < 3 & !FALSE ? c(1, 2) : c(9, 8, 7), c(1, 2))
   expect_equal(6 == 6 & 6 != 9 ? 'this' : 'that', 'this')
   expect_equal(is.character('a') & is.null(NULL) ? FALSE : TRUE , FALSE)
 })
@@ -25,21 +25,21 @@ test_that("`?` evaluates multi-part TRUE expressions correctly", {
 
 test_that("`?` evaluates simple FALSE cases correctly", {
   expect_equal(FALSE ? 1 : 2, 2)
-  expect_equal(FALSE ? c(1, 2) : c(9, 8), c(9, 8))
+  expect_equal(FALSE ? c(1, 2) : c(9, 8, 7), c(9, 8, 7))
   expect_equal(FALSE ? 'this' : 'that', 'that')
   expect_equal(FALSE ? FALSE : TRUE , TRUE)
 })
 
 test_that("`?` evaluates simple FALSE expressions correctly", {
   expect_equal(3 < 1 ? 1 : 2, 2)
-  expect_equal(1 > 3 ? c(1, 2) : c(9, 8), c(9, 8))
+  expect_equal(1 > 3 ? c(1, 2) : c(9, 8, 7), c(9, 8, 7))
   expect_equal(6 != 6 ? 'this' : 'that', 'that')
   expect_equal(is.character(1) ? FALSE : TRUE , TRUE)
 })
 
 test_that("`?` evaluates multi-part FALSE expressions correctly", {
   expect_equal(3 > 1 & is.numeric('a') ? 1 : 2, 2)
-  expect_equal(1 < 3 & FALSE ? c(1, 2) : c(9, 8), c(9, 8))
+  expect_equal(1 < 3 & FALSE ? c(1, 2) : c(9, 8, 7), c(9, 8, 7))
   expect_equal(6 > 6 | 6 == 9 ? 'this' : 'that', 'that')
   expect_equal(is.character('a') & is.call(NULL) ? FALSE : TRUE , TRUE)
 })
@@ -49,7 +49,6 @@ test_that("`?` can be used in `<-` assignment when fed truthy expressions", {
   expect_equal(x, 1)
   y <- 1 ? 2 : 3
   expect_equal(y, 2)
-  # TRUE == 1 is truthy
   z <- TRUE == 1 ? "foo" : NULL
   expect_equal(z, "foo")
 })
@@ -64,18 +63,15 @@ test_that("`?` can be used in `<-` assignment when fed falsy expressions", {
 })
 
 test_that("`?` can be used in `=` assignment when fed truthy expressions", {
-  # skip('not yet implemented')
   x = TRUE ? 1 : 2
   expect_equal(x, 1)
   y = 1 ? 2 : 3
   expect_equal(y, 2)
-  # TRUE == 1 is truthy
   z = TRUE == 1 ? "foo" : NULL
   expect_equal(z, "foo")
 })
 
 test_that("`?` can be used in `=` assignment when fed falsy expressions", {
-  # skip('not yet implemented')
   x = FALSE ? 1 : 2
   expect_equal(x, 2)
   y = 0 ? 2 : 3
@@ -83,3 +79,26 @@ test_that("`?` can be used in `=` assignment when fed falsy expressions", {
   z = TRUE == !1 ? "foo" : "bar"
   expect_equal(z, "bar")
 })
+
+test_that("`?` can be passed a logical vector", {
+  expect_equal(c(3, 5, 3, 5) > 4 ? "boof" : "foob",
+               c("foob", "boof", "foob", "boof"))
+  expect_equal(c(TRUE, FALSE, TRUE, FALSE) ? "true" : "fales",
+               c("true", "fales", "true", "fales"))
+  expect_equal(c("this", "that", "this", "that") == "this" ? "true" : "fales",
+               c("true", "fales", "true", "fales"))
+})
+
+test_that("`?` can be passed a logical matrix", {
+  skip("not yet implemented")
+  X <- matrix(rep(c(TRUE, FALSE, TRUE), 3), ncol = 3, byrow = TRUE)
+  Y <- matrix(rep(c("boof", "foob", "boof"), 3), ncol = 3, byrow = TRUE)
+  expect_equal(X ? "boof" : "foob", Y)
+})
+
+test_that("`?` statements can be chained with brackets", {
+  skip("not yet implemented")
+  x <- FALSE ? 1 : (FALSE ? 2 : (TRUE ? 3 : 4))
+  expect_equal(x, 3)
+})
+
